df777f5ca19ad1db740c1643640488d8e0d73305
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-01 13:20:10 +0200

    feat: implement store() method

---
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\Api\V1\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\HasApiTokens;
+
+class RegisterController extends Controller
+{
+    public function store(RegisterRequest $request)
+    {
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->accessToken;
+
+        return response()->json([
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+            ],
+            'token' => $token,
+        ], 201);
+    }
+}
