19446e79b313dd12ef5cdc568b2f13a2e701bfd3
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-01 18:41:56 +0200

    feat: add login route

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
 Route::post('/v1/register', [RegisterController::class, 'store']);
+
+Route::post('/v1/login', [LoginController::class, 'store']);
