d8e608a16ef26c98ef3f54f5900fb7bd12d08e8c
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-04 12:42:09 +0200

    feat: add put /me route and update get /me to use ProfileController

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\Auth\LogoutController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Api\V1\ProfileController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -17,4 +17,6 @@ Route::post('/v1/login', [LoginController::class, 'store']);
 
 Route::middleware('auth:api')->post('/v1/logout', [LogoutController::class, 'store']);
 
-Route::middleware('auth:api')->get('/v1/me', [ProfileController::class, 'store']);
+Route::middleware('auth:api')->get('/v1/me', [ProfileController::class, 'show']);
+
+Route::middleware('auth:api')->put('/v1/me', [ProfileController::class, 'update']);
