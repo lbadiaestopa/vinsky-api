22747405107644e35a28761d6f6e97cf55cbf348
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-04 13:01:08 +0200

    feat: add rules for validating new password

---
+++ app/Http/Requests/Api/V1/Profile/UpdatePasswordRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\Profile;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
+
+class UpdatePasswordRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'current_password' => ['required', 'string'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+    }
+
+    public function withValidator($validator): void
+    {
+        $validator->after(function ($validator) {
+            $user = $this->user();
+
+            if (! Hash::check($this->current_password, $user->password)) {
+                $validator->errors()->add(
+                    'current_password',
+                    'The current password is incorrect.'
+                );
+            }
+        });
+    }
+}
