eba83673dd657ad901bdea130de69fe148ddc439
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-04 13:04:58 +0200

    feat: implement updatePassword() method

--- app/Services/Profile/ProfileService.php
+++ app/Services/Profile/ProfileService.php
@@ -3,12 +3,22 @@
 namespace App\Services\Profile;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileService
 {
     public function update(User $user, array $data): User
     {
         $user->update($data);
+
+        return $user;
+    }
+
+    public function updatePassword(User $user, string $password): User
+    {
+        $user->update([
+            'password' => Hash::make($password),
+        ]);
 
         return $user;
     }
