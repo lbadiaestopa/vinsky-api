1b8d05cae9ebf35148ae46d31166f8f1a3368957
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-05 19:56:07 +0200

    feat: add policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Orchestra;
+use App\Policies\OrchestraPolicy;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,4 +23,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    protected $policies = [
+        Orchestra::class => OrchestraPolicy::class,
+    ];
 }
