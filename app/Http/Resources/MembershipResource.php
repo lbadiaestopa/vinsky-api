d72b7f9c016243d26a8c641f9701fe5058dbe96a
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-08 13:36:16 +0200

    feat: avoid errors by adding nullsafe access operator

--- app/Http/Resources/MembershipResource.php
+++ app/Http/Resources/MembershipResource.php
@@ -11,15 +11,15 @@ class MembershipResource extends JsonResource
     {
         return [
             'id' => $this->id,
-
+            
             'user' => [
-                'id' => $this->user->id,
-                'email' => $this->user->email,
+                'id' => $this->user?->id,
+                'email' => $this->user?->email,
             ],
 
             'orchestra' => [
-                'id' => $this->orchestra->id,
-                'name' => $this->orchestra->name,
+                'id' => $this->orchestra?->id,
+                'name' => $this->orchestra?->name,
             ],
 
             'role' => $this->role,
@@ -32,7 +32,6 @@ class MembershipResource extends JsonResource
 
             'joined_at' => $this->joined_at,
 
-            'created_at' => $this->created_at,
         ];
     }
 }
