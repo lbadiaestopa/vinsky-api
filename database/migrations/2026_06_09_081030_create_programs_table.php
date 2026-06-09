59d9a1d2be2a00ef09ea7096579e08bba3743fa2
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-09 11:12:17 +0200

    feat: add database factory, migration and seeder

---
+++ database/migrations/2026_06_09_081030_create_programs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('orchestra_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('name');
+
+            $table->date('start_date');
+            $table->date('end_date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('programs');
+    }
+};
