e6c39f37a98d48c12a42de44ece7f8c1bd48a7c2
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-09 11:12:59 +0200

    feat: add fillable fields

---
+++ app/Models/Program.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Program extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProgramFactory> */
+    use HasFactory;
+
+    protected $fillable = ['name', 'start_date', 'end_date', 'orchestra_id'];
+}
