9bb8b92d71742a78cea72d09d0ef1ecf99ea7914
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-09 17:12:40 +0200

    feat: add update() method

--- app/Http/Controllers/Api/V1/ProgramController.php
+++ app/Http/Controllers/Api/V1/ProgramController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Program\StoreProgramRequest;
+use App\Http\Requests\Api\V1\Program\UpdateProgramRequest;
 use App\Http\Resources\ProgramResource;
 use App\Models\Orchestra;
 use App\Models\Program;
@@ -20,8 +21,6 @@ class ProgramController extends Controller
 
     public function store(StoreProgramRequest $request, Orchestra $orchestra)
     {
-        $this->authorize('create', [Program::class, $orchestra]);
-
         return $this->service->create($request->validated(), $orchestra);
     }
 
@@ -38,6 +37,16 @@ class ProgramController extends Controller
     {
         $this->authorize('view', $program);
         
+        return new ProgramResource($program);
+    }
+
+    public function update(UpdateProgramRequest $request, Program $program)
+    {
+        $program = $this->service->update(
+            $program,
+            $request->validated(),
+        );
+
         return new ProgramResource($program);
     }
 }
