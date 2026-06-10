6f7a03d3e2ad3fede434574e5009514f8e33abfd
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-10 12:30:55 +0200

    feat: add basic controller structure and implement store() method

---
+++ app/Http/Controllers/Api/V1/EventController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\Event\StoreEventRequest;
+use App\Http\Resources\EventResource;
+use App\Models\Program;
+use App\Services\Events\EventService;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+
+class EventController extends Controller
+{
+    use AuthorizesRequests;
+
+    public function __construct(
+        private EventService $service
+    ) {}
+
+    public function store(StoreEventRequest $request, Program $program)
+    {
+        return $this->service->create($request->validated(), $program);
+    }
+}
