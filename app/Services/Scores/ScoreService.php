e28d9cccf9e687e7df546d030c1350584f510315
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-06-11 12:21:47 +0200

    feat: add store logic

---
+++ app/Services/Scores/ScoreService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services\Scores;
+
+use App\Models\Program;
+use App\Models\Score;
+use Illuminate\Http\UploadedFile;
+
+class ScoreService
+{
+    public function create(array $data, Program $program): Score
+    {
+        /** @var UploadedFile $file */
+        $file = $data['file'];
+
+        $mimeType = $file->getMimeType();
+
+        $path = $file->store('scores', 'public');
+
+        return Score::create([
+            'program_id' => $program->id,
+            'title' => $data['title'],
+            'file_path' => $path,
+            'original_name' => $file->getClientOriginalName(),
+            'size' => $file->getSize(),
+            'mime_type' => $mimeType,
+        ]);
+    }
+}
